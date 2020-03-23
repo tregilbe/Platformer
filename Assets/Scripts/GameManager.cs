f4fdcfb0e791305063e7303277285c350189cc78
Trenton <tregilbe@uat.edu>
2020-03-23 14:20:55 -0500

    Added ways of loading scenes in game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int currentScene = 0;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Warning: A second game manager was detected and destroyed.");
+        }
+    }
+
+    public void LoadLevel(string levelToLoad)
+    {
+        SceneManager.LoadScene(levelToLoad);
+        // Potentially Dangerous
+        currentScene++;
+    }
+
+    public void loadLevel(int indexToLoad)
+    {
+        SceneManager.LoadScene(indexToLoad);
+        currentScene = indexToLoad;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentScene + 1);
+    }
+}
